741e695d947a932e73bc24aa4e9aa03698397c4b
samrait1 <samrawitosmanworku@gmail.com>
2023-11-18 21:36:33 +0300

    first commit

---
+++ Anotherhello .java
@@ -0,0 +1,7 @@
+public class ternary {
+    public static void main(String[] args) {
+        int time=20;
+        String result=(time < 10) ? "good day" : "bad day";
+        System.out.print(result);
+    }
+}
